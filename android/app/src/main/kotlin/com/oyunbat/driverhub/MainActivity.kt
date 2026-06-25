a7ec15a08a24dca110ebfa9fa5dbeb2d3bceb92c
mac <mac@macs-iMac.local>
2026-06-25 11:58:41 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/oyunbat/driverhub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.oyunbat.driverhub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'DriverHub',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Test")),
+        body: const Center(
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
